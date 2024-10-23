af36f848023cb60a624c3b25160a7453b0f919b2
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-10-22 21:38:07 -0400

    updates on some components

--- src/components/AuthNavbar.jsx
+++ src/components/AuthNavbar.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/authnavbar.css'; // AuthNavbar-specific CSS
 
 const AuthNavbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="authnavbar">
-      <div className="authnavbar-left">
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/guest">Continue as Guest</Link>
-      </div>
-      
       <div className="authnavbar-center">
         <p>Meadin Fashion</p>
+        <div className="menu-icon" onClick={toggleMenu}>
+          {menuOpen ? '✖' : '☰'} {/* X icon when open, three dots when closed */}
+        </div>
+      </div>
+
+      <div className={`authnavbar-links ${menuOpen ? 'show' : ''}`}>
+        <div className="authnavbar-left">
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+         
+        </div>
       </div>
     </nav>
   );
--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/mainnavbar.css'; // Updated CSS file
 
 const MainNavbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="mainnavbar">
-      <div className="mainnavbar-left">
-        <Link to="/men">Men</Link>
-        <Link to="/women">Women</Link>
-        <Link to="/kaba">Kaba</Link>
-      </div>
-      
       <div className="mainnavbar-center">
         <p>Meadin Fashion</p>
+        <div className="menu-icon" onClick={toggleMenu}>
+          {menuOpen ? '✖' : '☰'} {/* X icon when open, three dots when closed */}
+        </div>
       </div>
+      
+      <div className={`mainnavbar-links ${menuOpen ? 'show' : ''}`}>
+        <div className="mainnavbar-left">
+          <Link to="/men">Men</Link>
+          <Link to="/women">Women</Link>
+          <Link to="/kaba">Kaba</Link>
+        </div>
 
-      <div className="mainnavbar-right">
-        <Link to="/home">Home</Link>
-        <Link to="/cart">Cart</Link>
+        <div className="mainnavbar-right">
+          <Link to="/home">Home</Link>
+          <Link to="/cart">Cart</Link>
+        </div>
       </div>
     </nav>
   );

9bbcd454073ce57c99ac631f655e5350e5f90b4f
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-11-03 14:36:12 -0500

    some name changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/M-Habesha-Market/'  // This should match your GitHub repository name
+  base: '/MHabesha/'  // This should match your GitHub repository name
 })
